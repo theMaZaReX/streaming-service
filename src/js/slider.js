60c76811df1f48492084d0b05413e7eb4a991d40
Dmitry <dmitri446@gmail.com>
2020-12-08 12:11:45 +0300

    adaptive header and listing

--- src/js/slider.js
+++ src/js/slider.js
@@ -6,7 +6,16 @@ $(document).ready(function(){
         slidesToScroll: 1,
         variableWidth:true,
         arrows:false,
-        focusOnSelect: false
+        focusOnSelect: false,
+        adaptiveHeight: true,
+        responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow:1
+            }
+          }
+        ]
       }
     )
   });
