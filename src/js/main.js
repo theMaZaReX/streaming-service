c36f0a9f59951b0c432ef7ba2e27288a513bd0f0
Dmitry <dmitri446@gmail.com>
2020-12-09 12:07:25 +0300

    adaptive search on mobile version with js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 const moviesName = document.querySelectorAll('.movies-list__name');
-
+const searchBtn = document.querySelector('.search__button');
+const searchInput = document.querySelector('.search__input');
+const headerInner = document.querySelector('.header__inner');
+let searchInputOpenned = false; 
 
 document.addEventListener("DOMContentLoaded", function(){
     moviesName.forEach(function(elem){
@@ -10,3 +13,16 @@ document.addEventListener("DOMContentLoaded", function(){
    
     })
 });
+
+const openSearchInput = function(){
+    if(!searchInputOpenned){
+    searchInputOpenned = true;
+    event.preventDefault();
+    headerInner.classList.add('header__inner--search-open');
+    searchInput.classList.add('search__input--open');
+    searchBtn.removeEventListener('click', openSearchInput);
+   
+    }
+}
+
+searchBtn.addEventListener("click", openSearchInput);
